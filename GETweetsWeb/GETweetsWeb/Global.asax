8c57d9ce125035002dc9e319dd3e865eb0f666fd
Galiya Warrier <galiya@galiya.info>
2015-03-24 14:36:12 +0000

    Working on initial version of the webpage (based on the mockup). Added chart into a carousel

---
+++ GETweetsWeb/GETweetsWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GETweetsWeb.MvcApplication" Language="C#" %>
---
+++ GETweetsWeb/GETweetsWeb/Models/PredictionResult.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GETweetsWeb.Models
+{
+    public class PredictionModelResult 
+    {
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public int Value { get; set; }
+
+    }
+
+}
